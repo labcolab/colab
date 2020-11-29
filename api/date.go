5d75b941024dad609dacf00df1019da1837d57fd
Harkunwar Singh Kochar <geek@harkunwar.com>
2020-11-29 14:43:34 -0800

    make requesthandler more express like

--- api/date.go
+++ api/date.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/labcolab/colab/lib/handlers/request_handler"
+	"github.com/labcolab/colab/lib/handlers/requesthandler"
 )
 
 // /date/now --> now
@@ -14,27 +14,31 @@ import (
 //need to make it so that /date/* to go to one function --> need to condense subURLs!
 func Date(w http.ResponseWriter, r *http.Request) {
 
-	rh := request_handler.New(r)
+	rh := requesthandler.New(r)
 
-	rh.Handle("GET", "/date/foo", func() {
+	rh.Get("/date/foo", func() {
 		fmt.Fprintf(w, "foo!")
 	})
 
-	rh.Handle("GET", "/date/bar", func() {
+	rh.Get("/date/bar", func() {
 		fmt.Fprintf(w, "bar!")
 	})
 
-	rh.Handle("GET", "/currentDate", func() {
+	rh.Get("/date/hello", func() {
+		fmt.Fprintf(w, "hello!")
+	})
+
+	rh.Get("/currentDate", func() {
 		currentTime := time.Now().Format(time.RFC850)
 		fmt.Fprintf(w, currentTime)
 	})
 
-	rh.Handle("GET", "/api/date", func() {
+	rh.Get("/api/date", func() {
 		currentTime := time.Now().Format(time.RFC850)
 		fmt.Fprintf(w, currentTime)
 	})
 
-	rh.Handle("GET", "/api/date", func() {
+	rh.Get("/api/date", func() {
 		fmt.Fprintf(w, "Duplicate - Should not be printed")
 	})
 }
---
+++ lib/handlers/requesthandler/requesthandler.go
@@ -0,0 +1,60 @@
+package requesthandler
+
+import "net/http"
+
+type requestCallback func()
+type methodCall func(path string, callback requestCallback)
+
+type RequestHandler struct {
+	Request         *http.Request
+	RequestComplete bool
+	Get             methodCall
+	Post            methodCall
+	Put             methodCall
+	Delete          methodCall
+	Patch           methodCall
+}
+
+func checkMethod(method string, request *http.Request) bool {
+	return request.Method == method
+}
+
+func checkPath(path string, request *http.Request) bool {
+	// TODO: Handle arguments like /user/:id
+	return path == request.URL.Path
+}
+
+func handleRequest(path string, rh *RequestHandler, callback requestCallback) {
+
+	if !rh.RequestComplete {
+
+		request := rh.Request
+
+		if !checkPath(path, request) {
+			return
+		}
+
+		callback()
+
+		rh.RequestComplete = true
+	}
+
+}
+
+func createMethodClosure(rh *RequestHandler, method string) methodCall {
+	return func(path string, callback requestCallback) {
+		if checkMethod(method, rh.Request) {
+			handleRequest(path, rh, callback)
+		}
+	}
+}
+
+func New(request *http.Request) *RequestHandler {
+	rh := RequestHandler{Request: request, RequestComplete: false}
+	rh.Get = createMethodClosure(&rh, "GET")
+	rh.Post = createMethodClosure(&rh, "POST")
+	rh.Put = createMethodClosure(&rh, "PUT")
+	rh.Patch = createMethodClosure(&rh, "PATCH")
+	rh.Delete = createMethodClosure(&rh, "DELETE")
+	return &rh
+}
