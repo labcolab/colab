68fd24e5ddfed2552a07d36474b9c67b9d16c183
gokcedilek <gokcedilek99@gmail.com>
2020-11-28 16:40:27 -0800

    go routes poc

--- api/date.go
+++ api/date.go
@@ -1,12 +1,24 @@
-package handler
+package date
 
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+// /date/now --> now
+// /date/foo --> foo
+
+//need to make it so that /date/* to go to one function --> need to condense subURLs!
+
+func Date(w http.ResponseWriter, r *http.Request) {
+	//todo switch
+	if r.URL.Path == "/date/foo" {
+		fmt.Fprintf(w, "foo!")
+	} else if r.URL.Path == "/date/bar" {
+		fmt.Fprintf(w, "bar!")
+	} else {
+		fmt.Fprintf(w, r.URL.Path)
+	}
+	// currentTime := time.Now().Format(time.RFC850)
+	// fmt.Fprintf(w, currentTime)
 }
