0bcffe1cd35185c64b0cdc9202319081d2055020
Harkunwar Kochar <geek@harkunwar.com>
2020-12-09 21:42:25 -0800

    Test cypress (#5)
    * cypress add
    
    * cypress test

---
+++ cypress/integration/test-page/date.spec.js
@@ -0,0 +1,14 @@
+context('Actions', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/date', {
+      statusCode: 200,
+      body: 'December 21',
+    });
+
+    cy.visit('http://localhost:3000/test');
+  });
+
+  it('Load date correctly from go', () => {
+    cy.get('[data-test-id="date"]', { timeout: 10000 }).contains('December');
+  });
+});
