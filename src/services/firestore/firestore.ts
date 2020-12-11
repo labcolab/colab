b6a34ec39e0d21107c6ffbe555b2e73ac7c13fae
gokcedilek <gokcedilek99@gmail.com>
2020-12-10 18:56:29 -0800

    first try env vars

--- src/services/firestore/firestore.ts
+++ src/services/firestore/firestore.ts
@@ -1,10 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
 enum Collections {
