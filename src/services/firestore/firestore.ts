9978fc58ef77c44845927d627d9054169e5c8138
gokcedilek <gokcedilek99@gmail.com>
2020-12-10 19:04:23 -0800

    second try

--- src/services/firestore/firestore.ts
+++ src/services/firestore/firestore.ts
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
-// };
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
 };
+// const firebaseConfig = {
+//   apiKey: window.env.FIREBASE_API_KEY,
+//   authDomain: window.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: window.env.FIREBASE_PROJECT_ID,
+// };
 
 enum Collections {
   Projects = 'projects',
