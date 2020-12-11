122dc24661d5da4781de9af2488395666cf71e05
Harkunwar Kochar <geek@harkunwar.com>
2020-12-10 20:17:19 -0800

    storybook publish (#7)
    * storybook publish
    
    * update storybook to latest version
    
    * make it shut up
    
    * redirect
    
    * change from url
    
    * fix storybook
    
    * remove stoires

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import React, { createContext } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import type firebase from 'firebase/app';
 import Home from './pages/Home/Home';
 import Welcome from './pages/Welcome/Welcome';
 import CreateProject from './pages/CreateProject/CreateProject';
 import Test from './pages/Test/Test';
 import Firestore from './services/firestore/firestore';
-import type firebase from 'firebase/app';
 import 'firebase/firestore';
 
 export const FirestoreContext = createContext<firebase.firestore.Firestore>(
   Firestore,
 );
 
-const App = () => {
-  return (
-    <FirestoreContext.Provider value={Firestore}>
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route path="/dashboard" component={Home} />
-        <Route path="/create" component={CreateProject} />
-        <Route path="/test" component={Test} />
-      </Switch>
-    </FirestoreContext.Provider>
-  );
-};
+const App = () => (
+  <FirestoreContext.Provider value={Firestore}>
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/dashboard" component={Home} />
+      <Route path="/create" component={CreateProject} />
+      <Route path="/test" component={Test} />
+      <Route
+        path="/stories"
+        component={() => {
+          window.location.href = '/storybook/';
+          return null;
+        }}
+      />
+    </Switch>
+  </FirestoreContext.Provider>
+);
 
 export default App;
