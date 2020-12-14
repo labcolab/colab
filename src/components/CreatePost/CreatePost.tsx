84f5d90a19ce5d7f6de35b59583a9fcc5ab2865d
gokcedilek <gokcedilek99@gmail.com>
2020-12-13 19:54:31 -0800

    clear chosen roles after submitting form

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -69,6 +69,7 @@ const CreatePost = () => {
           console.log(`error saving doc: ${err}`);
         }
         resetForm();
+        setSelectedRoles(defaultSelectedRoles);
       }}
     >
       {(props) => (
