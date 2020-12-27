54acaeb428ef72f8dd1dd940b22c70cce7a4e098
gokcedilek <gokcedilek99@gmail.com>
2020-12-27 15:24:04 -0800

    remove create project page from app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Welcome from './pages/Welcome/Welcome';
-import CreateProject from './pages/CreateProject/CreateProject';
 import Test from './pages/Test/Test';
 import FirebaseStuff, { FirebaseContext } from './services/firebase/firebase';
 
@@ -11,7 +10,6 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Welcome} />
       <Route path="/dashboard" component={Home} />
-      <Route path="/create" component={CreateProject} />
       <Route path="/test" component={Test} />
       <Route
         path="/stories"
