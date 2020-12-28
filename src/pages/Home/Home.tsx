138860caa1a73e91b978877fa90936a99c5e13bf
gokcedilek <gokcedilek99@gmail.com>
2020-12-28 09:10:56 -0800

    clean up old code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import { Container } from '@chakra-ui/react';
 import React from 'react';
 
 export default function HomePage() {
@@ -6,13 +5,5 @@ export default function HomePage() {
     <div className="container">
       <h1>Home Page</h1>
     </div>
-    // <Container w="100%" h="100%">
-    //   <StyledNavContainer>
-    //     <Navbar />
-    //   </StyledNavContainer>
-    //   <StyledFeedContainer className="form">
-    //     <Feed />
-    //   </StyledFeedContainer>
-    // </Container>
   );
 }
