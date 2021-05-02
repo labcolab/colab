1fd3f23568b8647a4ef3d8710e83f54c6f5d01d9
gokcedilek <gokcedilek99@gmail.com>
2021-05-02 11:33:24 -0700

    added signout page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Landing from './pages/Landing/Landing';
 import SignIn from './pages/SignIn/SignIn';
+import SignOut from './pages/SignOut/SignOut';
 import SignUp from './pages/SignUp/SignUp';
 import Test from './pages/Test/Test';
 import { AuthContext } from './services/auth/auth';
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/dashboard" component={Home} />
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/signout" component={SignOut} />
       <Route path="/test" component={Test} />
       <Route
         path="/stories"
